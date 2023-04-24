4cedf21f903c4adf05adf416ab116b375e0f2085
Breland <claybreland1@gmail.com>
2023-04-24 08:40:42 -0500

    added passport and others for auth

--- server/server.js
+++ server/server.js
@@ -7,38 +7,56 @@ var __importDefault =
 Object.defineProperty(exports, '__esModule', { value: true });
 
 
-require('dotenv').config({ path: '../.env' });
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-/* const express = require('express');
-const DeckModel = require("./models/Deck.js");
-const CardModel = require("./models/Card.js");
-const cors = require('cors')
-const Deck = DeckModel; */
 const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/database.js');
-
-
-dotenv.config({ path: './config/.env' });
 const app = express();
+const cors = require('cors');
 
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
+
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database.js');
 const deckRoutes = require('./routes/deckRoutes');
 const cardRoutes = require('./routes/cardRoutes');
+
+require('dotenv').config({ path: './config/.env' });
+
+// Passport config
+require('./config/passport')(passport)
+
+connectDB();
 
 app.use(cors());
 app.use(express.static('client/public'));  // Serve the React app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // this allows json post requests to be read by express
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ client: mongoose.connection.getClient() }),
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+
+
 
 app.use('/decks', deckRoutes);
 app.use('/cards', cardRoutes);
 
 
 
-
-connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running -- ${process.env.PORT}`)
-    });
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running -- ${process.env.PORT}`)
 });
