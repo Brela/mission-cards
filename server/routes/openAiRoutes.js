2036539f5c2cbcf8a31cdd4d4ab85b6057866e8a
Breland <claybreland1@gmail.com>
2023-04-29 08:09:14 -0500

    let's goo

--- server/routes/openAiRoutes.js
+++ server/routes/openAiRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const openAiController = require('../controllers/openAi');
 const { ensureAuth } = require('../middleware/auth')
 
+// let's goo
 router.post('/', ensureAuth, openAiController.sendPrompt);
 
 module.exports = router;
