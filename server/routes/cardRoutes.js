c774409a4c8e9558c6454e634aad7289d8f1f6d7
Breland <claybreland1@gmail.com>
2023-05-09 17:53:25 -0500

    temp rem ensureAuth

--- server/routes/cardRoutes.js
+++ server/routes/cardRoutes.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const cardController = require('../controllers/card');
 const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', ensureAuth, cardController.getAllCards);
+router.get('/', cardController.getAllCards);
+router.get('/:deckName', cardController.getAllCardsForDeck);
+router.get('/:deckName/:cardId', cardController.getSingleCard);
+router.post('/:deckName/newcard', cardController.createCard);
+router.delete('/:deckName/:cardId', cardController.deleteCard);
+router.put('/:deckName/:cardId', cardController.updateCard);
+
+/* router.get('/', ensureAuth, cardController.getAllCards);
 router.get('/:deckName', ensureAuth, cardController.getAllCardsForDeck);
 router.get('/:deckName/:cardId', ensureAuth, cardController.getSingleCard);
 router.post('/:deckName/newcard', ensureAuth, cardController.createCard);
 router.delete('/:deckName/:cardId', ensureAuth, cardController.deleteCard);
 router.put('/:deckName/:cardId', ensureAuth, cardController.updateCard);
-
+ */
 module.exports = router;
--- server/routes/openAiRoutes.js
+++ server/routes/openAiRoutes.js
@@ -4,6 +4,6 @@ const openAiController = require('../controllers/openAi');
 const { ensureAuth } = require('../middleware/auth')
 
 // ensure auth was not working here, so we will just block requests from the front end if user is not signed in
-router.post('/', ensureAuth, openAiController.sendPrompt);
+router.post('/', openAiController.sendPrompt);
 
 module.exports = router;
